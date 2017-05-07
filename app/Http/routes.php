9e8fd3f17bc6a529d95cc4dfd87a7e93683af7a8
yasukoyamashita <akubi0117@gmail.com>
2017-05-07 15:30:14 +0000

    user login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,5 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// ログイン認証
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
