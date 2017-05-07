03a53b129ea7b90e3b217e13e4a29d7b5ca674e5
yasukoyamashita <akubi0117@gmail.com>
2017-05-07 14:27:48 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
+
 @extends('layouts.app')
 
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
