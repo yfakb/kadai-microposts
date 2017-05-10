deabb92ace83bb2c8a8697aeb3d51849ced8b32b
yasukoyamashita <akubi0117@gmail.com>
2017-05-10 14:10:20 +0000

    user pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,3 +23,7 @@ Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
