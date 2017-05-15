03d36ba2e39f92e5b7ecc2d6d9c3caa0ea1278bc
yasukoyamashita <akubi0117@gmail.com>
2017-05-15 14:43:28 +0000

    test

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -8,7 +8,13 @@
         <div class="media-left">
             <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
         </div>
+        <div class="media-body">
+            <div>
+                {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
+            </div>
 
+            
+        </div>
     </li>
 @endforeach
 </ul>
