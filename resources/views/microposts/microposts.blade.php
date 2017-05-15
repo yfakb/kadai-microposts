7e1ad5d1ad9c6beb79df05ea5ab7f098d18edd4c
yasukoyamashita <akubi0117@gmail.com>
2017-05-15 14:45:49 +0000

    test

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -12,7 +12,17 @@
             <div>
                 {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
             </div>
-
+            <div>
+                <p>{!! nl2br(e($micropost->content)) !!}</p>
+            </div>
+            <div>
+                @if (Auth::user()->id == $micropost->user_id)
+                    {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                    {!! Form::close() !!}
+                @endif
+            </div>
+            
             
         </div>
     </li>
