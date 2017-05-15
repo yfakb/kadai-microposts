e125a3684903d227a8f4d3fd13db66daced6c577
yasukoyamashita <akubi0117@gmail.com>
2017-05-15 14:34:16 +0000

    test

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,6 +14,13 @@
                 
             </div>
         </aside>
-
+        <div class="col-xs-8">
+            <ul class="nav nav-tabs nav-justified">
+                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">Microposts <span class="badge">{{ $count_microposts }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
+            </ul>
+           
+        </div>
     </div>
 @endsection
