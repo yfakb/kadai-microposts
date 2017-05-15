40b181c66e2813c3833e13c2f2a09827791411ac
yasukoyamashita <akubi0117@gmail.com>
2017-05-15 14:41:38 +0000

    test

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -3,8 +3,13 @@
 
     <?php $user = $micropost->user; ?>
 
-テスト
 
+    <li class="media">
+        <div class="media-left">
+            <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
+        </div>
+
+    </li>
 @endforeach
 </ul>
 {!! $microposts->render() !!}
