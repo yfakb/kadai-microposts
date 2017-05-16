e8e37a5936606f37da7eb6bef95fddc9ba6f6cae
yasukoyamashita <akubi0117@gmail.com>
2017-05-16 14:00:17 +0000

    rename favorite

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::user()->id != $user->id) 
     @if (Auth::user()->is_favorite($micropost->id))
         {!! Form::open(['route' => ['user.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfollow', ['class' => "btn btn-warning btn-xs"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-warning btn-xs"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Follow', ['class' => "btn btn-success btn-xs"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-success btn-xs"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
